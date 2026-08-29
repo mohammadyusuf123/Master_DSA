9f1c1d24bd37faefec84e91c8291bac982b21917
Mohammad Yusuf <yusue786@gmail.com>
2026-08-29 15:39:55 +0600

    first commit

---
+++ Array/Array.js
@@ -0,0 +1,16 @@
+//find duplicate from an array
+
+// let arr = [3,2,1,3,5,4]
+
+// const findDuplicate = (arr=[],val) => {
+//     let x = 0
+//     for (let i = 0; i < arr.length; i++){
+//         if (arr[i] !=val) {
+//             arr[x] = arr[i]
+//             x = x + 1
+//         }
+//     }
+//    return x
+// }
+
+// console.log(findDuplicate(arr, 3))
