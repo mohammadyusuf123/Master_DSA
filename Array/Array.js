b54ddbd0ff01fa0f08e35b3addafefbbec8f68da
Mohammad Yusuf <yusue786@gmail.com>
2026-08-29 15:59:38 +0600

    fill with zero

--- Array/Array.js
+++ Array/Array.js
@@ -31,3 +31,26 @@
 // }
 
 // console.log(swipNum(arr))
+
+
+//fill with zero 
+
+// let nums = [0, 1, 4, 0, 6, 0]
+
+// const findZero = (nums = []) => {
+//     let x = 0;
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums[i] != 0) {
+//             nums[x] == nums[i]
+//             x++
+
+//         }
+
+//     }
+//     for (let i = x; i < nums.length; i++){
+//         nums[i]=0
+//     }
+//     return nums
+// }
+
+// console.log(findZero(nums))
